e12abb47e90260db6225b9d0cfb8f6291288a3a3
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-12-19 09:00:21 -0500

    add class notes and code

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
-const bookmarksController = require('./controllers/bookmarksController')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+// const bookmarks = require("./controllers/bookmarksController");
+const bookmarksController = require("./controllers/bookmarksController");
 
-app.use('/bookmarks', bookmarksController)
+app.use((req, res, next) => {
+  console.log("This code runs for every request");
+  next();
+});
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Bookmarks App')
-})
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use("/bookmarks", bookmarksController);
 
-app.get('*', (req, res) => {
-    res.status(404).json({ error: 'Page not found' })
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Bookmarks App");
+});
 
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
 
-module.exports = app
+module.exports = app;
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const bookmarks = express.Router()
-const bookmarksArray = require('../models/bookmark')
+const express = require("express");
+const bookmarks = express.Router();
+const { validateURL } = require("../models/validations");
+const bookmarksArray = require("../models/bookmark");
+
+bookmarks.use((req, res, next) => {
+  console.log("This middleware runs for EVERY bookmark route");
+  next();
+});
 
 // GET ROUTE FOR /bookmarks (app.use in app.js handles the entry point for the controller)
-bookmarks.get('/', (req, res) => {
-    res.json(bookmarksArray)
-})
+bookmarks.get("/", (req, res) => {
+  res.json(bookmarksArray);
+});
+
+// /bookmarks endpoint for POST
+bookmarks.post("/", validateURL, (req, res) => {
+  bookmarksArray.push(req.body);
+  res.json(bookmarksArray.at(-1));
+});
 
 // SHOW ROUTE
-bookmarks.get('/:index', (req, res) => {
-    const { index } = req.params
-    res.json(bookmarksArray[index])
-})
-
+bookmarks.get("/:index", (req, res) => {
+  const { index } = req.params;
+  if (bookmarksArray[index]) {
+    res.status(200).json(bookmarksArray[index]);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
 
 module.exports = bookmarks;
-
-
