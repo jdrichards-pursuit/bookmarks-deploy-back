403ea6946731684a94911606023412269fc900fa
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-12-19 11:01:35 -0500

    final code

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -8,7 +8,7 @@ bookmarks.use((req, res, next) => {
   next();
 });
 
-// GET ROUTE FOR /bookmarks (app.use in app.js handles the entry point for the controller)
+// GET ROUTE FOR /bookmarks (app.use in app.js handles the entry point for the controller) SHOW ALL BOOKMARKS
 bookmarks.get("/", (req, res) => {
   res.json(bookmarksArray);
 });
@@ -19,13 +19,28 @@ bookmarks.post("/", validateURL, (req, res) => {
   res.json(bookmarksArray.at(-1));
 });
 
-// SHOW ROUTE
+// SHOW ONE BOOKMARK BASED ON INDEX
 bookmarks.get("/:index", (req, res) => {
   const { index } = req.params;
   if (bookmarksArray[index]) {
     res.status(200).json(bookmarksArray[index]);
   } else {
     res.status(404).json({ error: "Not Found" });
+  }
+});
+
+bookmarks.delete("/:index", (req, res) => {
+  const deletedBookmark = bookmarksArray.splice(req.params.index, 1);
+  res.status(200).json(deletedBookmark);
+});
+
+bookmarks.put("/:index", (req, res) => {
+  const { index } = req.params;
+  if (bookmarksArray[index]) {
+    bookmarksArray[index] = req.body;
+    res.status(200).json(bookmarksArray[index]);
+  } else {
+    res.status(404).json({ message: "Not Found" });
   }
 });
 
