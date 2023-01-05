d865854b290d3cc1609cb2d4f13fc2d716e40b92
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-01-05 12:00:10 -0500

    update for new repo

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 // const bookmarks = require("./controllers/bookmarksController");
 const bookmarksController = require("./controllers/bookmarksController");
 
+//Middleware
 app.use((req, res, next) => {
   console.log("This code runs for every request");
   next();
 });
 
+app.use(cors());
+// express.json() relates specifically to POST routes
 app.use(express.json());
+
+// morgan displays
 app.use(morgan("tiny"));
 app.use("/bookmarks", bookmarksController);
 
